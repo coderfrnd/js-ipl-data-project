6fa20e2b0e15b5b86c90aa3756f5c3c8acbe88c7
Abhishek <warriorsshadow39@gmail.com>
2025-02-07 21:23:37 +0530

    first-problem-done

---
+++ src/data/file.js
@@ -0,0 +1,17 @@
+const fs = require("fs");
+const csv = require("csvtojson");
+
+// Function to convert CSV to JSON
+const convertCsvToJson = async (csvFile, jsonFile) => {
+  try {
+    const jsonArray = await csv().fromFile(csvFile);
+    fs.writeFileSync(jsonFile, JSON.stringify(jsonArray, null, 2));
+    console.log(`Converted ${csvFile} to ${jsonFile}`);
+  } catch (error) {
+    console.error("Error:", error);
+  }
+};
+
+// Convert both CSV files
+convertCsvToJson("matches.csv", "file1.json");
+convertCsvToJson("deliveries.csv", "file2.json");
---
+++ src/server/match-per-year.js
@@ -0,0 +1,22 @@
+const matches = require('../data/matches.json')
+const deliveries = require('../data/deliveries.json')
+// const fs = require('node:fs');
+const fs = require('fs');
+let countmatch = {}
+function numberofmatch(data){
+   
+     for(let key in data) {
+       (countmatch[data[key].season])? countmatch[data[key].season]++ : countmatch[data[key].season] = 1;
+     }
+}
+
+numberofmatch(matches)
+console.log(countmatch);
+fs.writeFile("../public/output/match-per-season.json",JSON.stringify(countmatch, null, 2), (err)=>{
+  if(err){
+    console.log("There is some error ", err);
+  }
+  else console.log("okk done ");
+});
+
+
