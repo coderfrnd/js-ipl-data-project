a6808a5eb7c3052621441c7992a8481277c951b1
Abhishek <warriorsshadow39@gmail.com>
2025-02-09 21:25:05 +0530

    DONE ALL QUESTION BUT MINOR CHANGES AND OPTIMIZATION REMANING

--- src/server/8-highest-number-of-dismissal.js
+++ src/server/8-highest-number-of-dismissal.js
@@ -8,14 +8,14 @@ function createbowlerdata(data){
      let profiledata = {}
 
     for(let key in data){
-          for( let bowlername in data[key] ){
+      
             let obj = data[key]
             
             if(obj["player_dismissed"]!=""){
                 let batsman = obj["batsman"]
                 let bowler =obj["bowler"]
                 if(profiledata[bowler]){
-                   if(profiledata[bowler][batsman]) profiledata[bowler][batsman]++;
+                   if(profiledata[bowler][batsman])profiledata[bowler][batsman]++;
                    else {
                     profiledata[bowler][batsman] = 1 
                    }
@@ -24,16 +24,47 @@ function createbowlerdata(data){
                     profiledata[bowler] = {
                         [batsman] : 1
                     }
-                }
-            }
-
-            
-          }
-          
+                }  
+          }   
     }
     return profiledata
-
 }
 
-let answer = createbowlerdata(deliveries)
-console.log(answer);
+let bowlerwicketdata = createbowlerdata(deliveries)
+
+function findmostdismisial(bowlerwicketdatadata){
+    let mostdismisial ={
+        "no-of-wicket":0
+        }
+    for(let key in bowlerwicketdata){
+       let singleobj = bowlerwicketdata[key]
+       let bowlername = key
+       let countofwicket =0
+    for(let batsman in singleobj){
+        // console.log(singleobj[batsman]);
+        if(singleobj[batsman]>mostdismisial["no-of-wicket"]) {
+            countofwicket = singleobj[batsman]
+
+            mostdismisial = {
+               [bowlername ]:batsman,
+                "no-of-wicket" : countofwicket
+            }
+        }
+   
+
+        
+        
+    }
+   
+    
+   
+    
+        
+    }
+    return mostdismisial
+}
+let mostdismisial= findmostdismisial(bowlerwicketdata)
+
+
+
+writeFile("8-highest-number-of-dismisial.json",JSON.stringify(mostdismisial,null,3))
