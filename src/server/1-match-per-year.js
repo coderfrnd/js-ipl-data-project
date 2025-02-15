aec5ae98bc4c990cde4df7a08170ac8fa65a6c3c
Abhishek <warriorsshadow39@gmail.com>
2025-02-15 10:11:42 +0530

    remove spaces

--- src/server/1-match-per-year.js
+++ src/server/1-match-per-year.js
@@ -1,22 +1,21 @@
-const matches = require('../data/matches.json')
-const deliveries = require('../data/deliveries.json')
-const writeFile = require('../data/writefile/write-file.js')
+const matches = require("../data/matches.json");
+const deliveries = require("../data/deliveries.json");
+const writeFile = require("../data/writefile/write-file.js");
 
-function numberofmatch(data){
-   
-let countmatch = {}
-     for(let key in data) {
+function numberofmatch(data) {
+  let countmatch = {};
+  for (let key in data) {
     // Here key is every single single individual object key and object is value
-      
-      if(countmatch[data[key].season]) countmatch[data[key].season]++ 
-      else countmatch[data[key].season] = 1;
 
-      // Here data[key].season give us every year information about which year then using countmatch object we calculate match
-     }
-     return countmatch
+    if (countmatch[data[key].season]) countmatch[data[key].season]++;
+    else countmatch[data[key].season] = 1;
+
+    // Here data[key].season give us every year information about which year then using countmatch object we calculate match
+  }
+  return countmatch;
 }
 
-let countmatch = numberofmatch(matches)
+let countmatch = numberofmatch(matches);
 // console.log(countmatch);
 // Writefile function a custom function which is take two argument one is name and second one is in which formate we want to convert
-writeFile("1-match-per-season.json",JSON.stringify(countmatch,null,3))
+writeFile("1-match-per-season.json", JSON.stringify(countmatch, null, 3));
--- src/server/3-extra-run-conceded-per-team.js
+++ src/server/3-extra-run-conceded-per-team.js
@@ -2,7 +2,7 @@ const matches = require("../data/matches.json");
 const deliveries = require("../data/deliveries.json");
 const writeFile = require("../data/writefile/write-file.js");
 
-function getmatchid(matches) {
+function getMatchid(matches) {
     let matchIdDetails = []
     for(let key in matches){
    if(matches[key].season == '2016'){
@@ -13,32 +13,26 @@ function getmatchid(matches) {
 }
 // This is a function which is used to count extra run conceded it takes two parameter first one matchperball dataa and second one is matchid which we extract from previous function getmatchid()
 
-function extrarunconceded(data, matchid) {
-  let extrarun = {}; // Intialize empty object to store data of extra-run
-let matchId = getmatchid(matches)
+function extraRunConceded(data) {
+  let extraRun = {}; // Intialize empty object to store data of extra-run
+let matchId = getMatchid(matches)
   matchId.forEach((num) => {
     for (let key in data) {
       if (data[key].match_id == num) {
-        if (extrarun[data[key].bowling_team]) {
+        if (extraRun[data[key].bowling_team]) {
           let convert = data[key].extra_runs;
-          extrarun[data[key].bowling_team]["extrarun"] += Number(convert);
-          // console.log(extrarun[data[key].bowling_team]);
+          extraRun[data[key].bowling_team]["extraRun"] += Number(convert);
         } else {
           let convert = data[key].extra_runs;
-          extrarun[data[key].bowling_team] = { extrarun: Number(convert) };
+          extraRun[data[key].bowling_team] = { extraRun: Number(convert) };
         }
       }
     }
   });
-  return extrarun;
+  return extraRun;
 }
-let matchid = getmatchid(matches);
-// console.log(matchid);
-
-let extrarundata = extrarunconceded(deliveries, matchid);
-// console.log(extrarun);
-
+let extraRunData = extraRunConceded(deliveries);
 writeFile(
   "3-extra-run-conceded-in-2016.json",
-  JSON.stringify(extrarundata, null, 3)
+  JSON.stringify(extraRunData, null, 3)
 );
